d632ce649c286fc3167655df968b3cb41dc14e47
konpalsrivastava <konpalsrivastava.21@gmail.com>
2026-08-05 11:55:33 +0530

    Add: daily-temperatures - submission-9

---
+++ Data Structures & Algorithms/daily-temperatures/submission-9.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        Stack<Integer> st = new Stack<>();
+        int[] ans = new int[temperatures.length];
+        Arrays.fill(ans,0);
+        for(int i=0;i<temperatures.length;i++){
+        while(!st.isEmpty() && temperatures[st.peek()]<temperatures[i]){
+            int day = st.pop();
+            ans[day] = i-day;
+        }
+        st.push(i);
+        }
+        return ans;
+    }
+}
