9bff7760b46be3a8abe2a14b66afca12e16619dc
konpalsrivastava <konpalsrivastava.21@gmail.com>
2026-08-15 11:15:43 +0530

    Add: minimum-number-of-swaps-to-make-the-string-balanced - submission-2

---
+++ Data Structures & Algorithms/minimum-number-of-swaps-to-make-the-string-balanced/submission-2.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int minSwaps(String s) {
+        int balance=0;
+        int swaps=0;
+        for(char c: s.toCharArray()){
+            if(c=='['){
+                balance++;
+            }
+            else{
+                balance--;
+            }
+            if(balance<0){
+                swaps++;
+                balance=1;
+            }
+        }
+        return swaps;
+    }
+}
