5a0376cc877824304ec209756cb741c27655ff03
konpalsrivastava <konpalsrivastava.21@gmail.com>
2026-08-18 09:39:15 +0530

    Add: permutation-string - submission-4

---
+++ Data Structures & Algorithms/permutation-string/submission-4.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        int[] freq1 = new int[26];
+        int[] freq2 = new int[26];
+        if(s1.length()>s2.length()) return false;
+        for(int i=0;i<s1.length();i++){
+            freq1[s1.charAt(i)-'a']++;
+        }
+        int k = s1.length();
+        for(int i=0;i<k;i++){
+            freq2[s2.charAt(i)-'a']++;
+        }
+        if(Arrays.equals(freq1,freq2)) return true;
+        for(int i=k;i<s2.length();i++){
+            freq2[s2.charAt(i-k)-'a']--;
+            freq2[s2.charAt(i)-'a']++;
+            if(Arrays.equals(freq1,freq2)) return true;
+        }
+        return false;
+    }
+}
