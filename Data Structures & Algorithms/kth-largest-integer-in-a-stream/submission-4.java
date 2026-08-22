cd60a4801be0b2846d6c112be9a43e40b217c625
konpalsrivastava <konpalsrivastava.21@gmail.com>
2026-08-22 19:54:52 +0530

    Add: kth-largest-integer-in-a-stream - submission-4

---
+++ Data Structures & Algorithms/kth-largest-integer-in-a-stream/submission-4.java
@@ -0,0 +1,23 @@
+class KthLargest {
+    int k;
+    PriorityQueue<Integer> minheap;
+    public KthLargest(int k, int[] nums) {
+        this.k=k;
+        minheap = new PriorityQueue<>();
+        for(int num:nums){
+            minheap.add(num);
+            if(minheap.size()>k){
+                minheap.poll();
+            }
+        }   
+    }
+    
+    public int add(int val) {
+        //minheap.add(val);
+        minheap.offer(val);
+        if(minheap.size()>k){
+            minheap.poll();
+        }
+        return minheap.peek();
+    }
+}
