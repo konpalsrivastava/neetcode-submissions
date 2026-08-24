bfeaf40ed4e4a9027a9b27dfd1207aad2c028a50
konpalsrivastava <konpalsrivastava.21@gmail.com>
2026-08-24 20:28:40 +0530

    Add: cheapest-flight-path - submission-2

---
+++ Data Structures & Algorithms/cheapest-flight-path/submission-2.java
@@ -0,0 +1,50 @@
+class Solution {
+    class pair{
+        int node;
+        int cost;
+        int stops;
+        pair(int node,int cost,int stops){
+            this.node=node;
+            this.cost=cost;
+            this.stops=stops;
+        }
+    }
+    public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
+       List<List<pair>> adj = new ArrayList<>();
+       for(int i=0;i<n;i++){
+        adj.add(new ArrayList<>());
+       }
+       for(int[] f:flights){
+        adj.get(f[0]).add(new pair(f[1],f[2],0));
+       }
+       int[] dist = new int[n+1];
+       Arrays.fill(dist,Integer.MAX_VALUE);
+       dist[src] = 0;
+       Queue<pair> q = new LinkedList<>();
+       q.offer(new pair(src,0,0));
+       while(!q.isEmpty()){
+        pair curr = q.poll();
+        int nnode = curr.node;
+        int c = curr.cost;
+        int s = curr.stops;
+
+        if(s>k){
+            continue;
+        }
+
+        for(pair itr:adj.get(nnode)){
+            int adjnode = itr.node;
+            int wt = itr.cost;
+
+            if(c+wt<dist[adjnode]){
+                dist[adjnode] = c+ wt;
+                q.offer(new pair(adjnode,c+wt,s+1));
+            }
+        }
+       }
+       if(dist[dst]==Integer.MAX_VALUE){
+        return -1;
+       }
+       return dist[dst];
+    }
+}
